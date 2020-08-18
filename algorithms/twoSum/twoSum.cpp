2c8d286b4e7a4c29a16c62b5e2d0346a62768a46
raymond <runxindong@gmail.com>
2020-08-18 18:26:47 +0800

    twoSum

---
+++ algorithms/twoSum/twoSum.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+		unordered_map<int, int> m;
+		vector<int> result;
+		for (int i = 0; i < nums.size(); i++){
+			if (m.find(target-nums[i]) == m.end()){
+				m[nums[i]] = i;
+			}else{
+				result.push_back(m[target-nums[i]]);
+				result.push_back(i);
+			}
+		}  
+		return result;
+    }
+};
+
+int main(){
+	vector<int> nums,result;
+	int target = 9;
+	Solution solution;
+	
+	nums.push_back(2);
+	nums.push_back(7);
+	nums.push_back(11);
+	nums.push_back(15);
+
+	result = solution.twoSum(nums,target);
+	vector<int>::iterator  it;
+	for (it = result.begin(); it != result.end(); it++){
+		cout << *it << endl;
+	}
+
+	return 0;
+}
